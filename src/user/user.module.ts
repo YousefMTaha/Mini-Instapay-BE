70e086d6781217fee3d80e20a84c8a62ce9c4012
Yousef Taha <tyousef262@gmail.com>
2024-11-28 18:53:59 +0200

    add mail service to user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { userModel } from 'src/schemas/user.schema';
+import { MailService } from 'src/utils/email.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService,MailService],
   imports: [userModel],
 })
 export class UserModule {}

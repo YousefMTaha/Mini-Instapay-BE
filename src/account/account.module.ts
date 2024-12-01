91b76902b77221e355b131358e7c2655ccdefe39
Yousef Taha <tyousef262@gmail.com>
2024-12-02 01:21:56 +0200

    link accountDB and userDB with account module

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AccountService } from './account.service';
 import { AccountController } from './account.controller';
+import { userModel } from 'src/schemas/user.schema';
+import accountModel from 'src/schemas/account.schema';
 
 @Module({
   controllers: [AccountController],
   providers: [AccountService],
+  imports: [accountModel, userModel],
 })
 export class AccountModule {}

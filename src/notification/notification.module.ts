a30100401a5e209cbb713719e0d4f92499715d84
Yousef Taha <tyousef262@gmail.com>
2024-12-07 16:18:12 +0200

    implement notification gateway

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
 import notificationModel from 'src/schemas/notification.schema';
+import { NotificationGateWay } from './notification.gateway';
 
 @Module({
   controllers: [NotificationController],
-  providers: [NotificationService],
+  providers: [NotificationService,NotificationGateWay],
   imports: [notificationModel],
   exports: [NotificationService],
 })

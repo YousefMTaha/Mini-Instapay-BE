6e1c8178f2a1b0eedb6147b354d9f1c680a12ac9
Yousef Taha <tyousef262@gmail.com>
2024-12-07 14:36:56 +0200

    fixed some bugs

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
+import notificationModel from 'src/schemas/notification.schema';
 
 @Module({
   controllers: [NotificationController],
   providers: [NotificationService],
+  imports: [notificationModel],
+  exports: [NotificationService],
 })
 export class NotificationModule {}

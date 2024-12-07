72a19628a5150f272a2bb352e3bf7b0fe2d646f6
Yousef Taha <tyousef262@gmail.com>
2024-12-07 14:10:04 +0200

    remove unneccasry code

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { userModel } from 'src/schemas/user.schema';
-import { MailService } from 'src/utils/email.service';
 import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, MailService],
-  imports: [userModel, AuthModule],
+  providers: [UserService],
+  imports: [AuthModule, userModel],
   exports: [UserService],
 })
 export class UserModule {}

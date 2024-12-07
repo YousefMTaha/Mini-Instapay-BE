9055fe55a2a8ba3f2a533049d4e2302768589e04
Yousef Taha <tyousef262@gmail.com>
2024-12-07 14:08:02 +0200

    modify add account

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -14,12 +14,16 @@ import { currentUser } from 'src/decorators/current-user.decortaor';
 import { userType } from 'src/schemas/user.schema';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { UnHandledExceptions } from 'src/filters/unhandeldErrors.filter';
+import { CardService } from 'src/card/card.service';
 
 @Controller('account')
 @UseGuards(AuthGuard)
 @UseFilters(UnHandledExceptions)
 export class AccountController {
-  constructor(private readonly accountService: AccountService) {}
+  constructor(
+    private readonly accountService: AccountService,
+    private readonly cardService: CardService,
+  ) {}
 
   @Get()
   getAllAccounts(@currentUser() user: userType) {
@@ -44,8 +48,10 @@ export class AccountController {
   }
 
   @Post()
-  add(@Body() body: any, @currentUser() user: userType) {
-    return this.accountService.addAccount(body, user);
+  async add(@Body() body: any, @currentUser() user: userType) {
+    await this.cardService.checkCardExist(body.cardNo);
+    const card = await this.cardService.addCard(body);
+    return this.accountService.addAccount(body, user, card.data);
   }
 
   // @Patch(':id')
